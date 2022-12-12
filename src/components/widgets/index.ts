e81ccd6e74f256e2b88ae5e872e2d9d70973a1e4
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2022-12-12 23:57:49 +0100

    export fix

--- src/components/widgets/index.ts
+++ src/components/widgets/index.ts
@@ -3,3 +3,4 @@ export * from "./ItemsList";
 export * from "./Table";
 export * from "./Table/components/TablePaginator";
 export * from "./Table/components/TableToolbar";
+export * from "./KeyValueBox/KeyValueBox";
