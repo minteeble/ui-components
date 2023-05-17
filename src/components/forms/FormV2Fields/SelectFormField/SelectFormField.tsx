88a0f0c4cee1a9d870c6161ac93806c1e9f00ded
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2023-05-17 12:00:22 +0200

    select form field init

--- src/components/forms/FormV2Fields/SelectFormField/SelectFormField.tsx
+++ src/components/forms/FormV2Fields/SelectFormField/SelectFormField.tsx
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SelectFormFieldProps } from "./SelectFormField.types";
 
 export const SelectFormField = (props: SelectFormFieldProps) => {
+  const options: string[] = props.attributes.options || [];
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [currentOption, setCurrentOption] = useState<number | null>(
+    parseInt(props.value) || null
+  );
+
+  useEffect(() => {
+    if (currentOption) {
+      props.setValue(currentOption);
+    }
+  }, [currentOption]);
+
   return (
     <div className="form-field select-form-field">
-      <input
-        className="montserrat"
-        value={props.value}
-        onChange={(e) => {
-          props.setValue(e.target.value);
+      <div
+        onClick={() => {
+          setIsOpen(!isOpen);
         }}
-        placeholder={props.placeholder || ""}
-      />
+        className={`montserrat select-field ${currentOption ? "" : "null"}`}
+        // onChange={(e) => {
+        //   props.setValue(e.target.value);
+        // }}
+      >
+        {currentOption ? options[currentOption] : props.placeholder || ""}
+      </div>
+      <div className={`options ${isOpen}`}>
+        {options.map((option, i) => {
+          return (
+            <div
+              className="option"
+              key={i}
+              onClick={() => {
+                setCurrentOption(i);
+              }}
+            >
+              {option}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
