e0c09fcca39ec6de80c5481469fe77364f585944
Samuele Girgenti <samuele.girgenti458@gmail.com>
2023-09-28 17:53:38 +0300

    Info card update

--- packages/ui-components/src/components/common/InfoCard/InfoCard.tsx
+++ packages/ui-components/src/components/common/InfoCard/InfoCard.tsx
@@ -1,6 +1,95 @@
-import React from "react";
-import { InfoCardProps } from "./InfoCard.types";
+import {
+  faTriangleExclamation,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { PageCard } from "../../page";
+import LoadingSpinner, { LoadingSpinnerSize } from "../LoadingSpinner";
+import { InfoCardProps, InfoCardSize, InfoCardType } from "./InfoCard.types";
 
 export const InfoCard = (props: InfoCardProps) => {
-  return <div className="info-card"></div>;
+  //States
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [isClosing, setIsClosing] = useState<boolean>(false);
+
+  const type = props.type ?? InfoCardType.Info;
+  const size = props.size ?? InfoCardSize.Normal;
+  const closable = props.closable ?? false;
+
+  //Methods
+  const handleClose = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      setIsActive(false);
+      setIsClosing(false);
+    }, 500);
+  };
+
+  const checkActiveState = () => {
+    console.log("Active:", props.active, typeof props.active);
+    if (props.active) {
+      setIsActive(props.active);
+    } else if (typeof props.active === "undefined") {
+      setIsActive(true);
+    } else {
+      handleClose();
+    }
+  };
+
+  //Effects
+  useEffect(() => {
+    checkActiveState();
+  }, [props.active]);
+
+  useEffect(() => {
+    checkActiveState();
+  }, []);
+
+  return (
+    <>
+      {isActive && (
+        <PageCard
+          className={`info-card type-${type} size-${size} ${
+            isClosing ? "close" : ""
+          } ${props.className || ""}`}
+          style={props.style}
+        >
+          <>
+            <div className="message-box">
+              <div className="icon-wrapper">
+                {type === InfoCardType.Loading ? (
+                  <>
+                    <LoadingSpinner Size={LoadingSpinnerSize.Medium} />
+                  </>
+                ) : (
+                  <FontAwesomeIcon
+                    className="icon"
+                    icon={faTriangleExclamation}
+                  />
+                )}
+              </div>
+              <span className="message-content">
+                {props.message || "Message"}
+              </span>
+            </div>
+            {closable ? (
+              <>
+                <span
+                  className="close"
+                  onClick={() => {
+                    handleClose();
+                  }}
+                >
+                  <FontAwesomeIcon icon={faXmark} />
+                </span>
+              </>
+            ) : (
+              <i></i>
+            )}
+          </>
+        </PageCard>
+      )}
+    </>
+  );
 };
--- packages/ui-components/src/components/common/InfoCard/InfoCard.types.ts
+++ packages/ui-components/src/components/common/InfoCard/InfoCard.types.ts
@@ -4,11 +4,31 @@ import { ClassExtensible, StylableComponent } from "../../../models";
  * Type of the Info card. Based on it, the style of the card will be adapted (colors, icons, etc.)
  */
 export enum InfoCardType {
-  Info,
-  Success,
-  Warning,
-  Error,
-  Loading,
+  Info = "info",
+  Success = "success",
+  Warning = "warning",
+  Error = "error",
+  Loading = "loading",
+}
+
+/**
+ * Size of the info card
+ */
+export enum InfoCardSize {
+  /**
+   * Info card will be rendererd by removign all the unnecessary paddings and spaces
+   */
+  Small = "small",
+
+  /**
+   * Info card will be rendered inside a `PageCard` without much padding and spaces, in order to fit into smaller layouts
+   */
+  Normal = "normal",
+
+  /**
+   * Info card will be rendered inside a normal `PageCard` component.
+   */
+  Large = "large",
 }
 
 /**
@@ -21,7 +41,22 @@ export interface InfoCardProps extends ClassExtensible, StylableComponent {
   type?: InfoCardType;
 
   /**
+   * Card layout size
+   */
+  size?: InfoCardSize;
+
+  /**
    * Card message. It can be either a raw type (like a string or number), or a custom JSX component
    */
   message?: JSX.Element | string | number;
+
+  /**
+   * Specifies if card is active or not
+   */
+  active?: boolean;
+
+  /**
+   * Enables the close button in the top-right corner of the card
+   */
+  closable?: boolean;
 }
