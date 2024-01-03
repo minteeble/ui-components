aa401486275f4b315303787304041dfe541480db
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-01-03 16:52:38 +0100

    fix css imports

--- packages/ui-components-extra/.storybook/preview.js
+++ packages/ui-components-extra/.storybook/preview.js
@@ -1,4 +1,9 @@
-import "!style-loader!css-loader!sass-loader!../src/style/main.scss";
+import "style-loader";
+import "./custom.scss";
+import "css.loader";
+import "sass-loader";
+import "../src/style/main.scss";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
