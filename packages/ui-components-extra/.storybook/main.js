d7e84aaa0fca72f617f12b891c4b6c015486b6ed
Samuele Girgenti <samuele.girgenti458@gmail.com>
2024-01-03 17:02:14 +0100

    Update

--- packages/ui-components-extra/.storybook/main.js
+++ packages/ui-components-extra/.storybook/main.js
@@ -1,11 +1,11 @@
+import { dirname, join } from "path";
 const path = require("path");
 module.exports = {
   stories: ["../src/**/**/*.stories.mdx", "../src/**/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"
-  // "@storybook/preset-scss",
-  , "@storybook/addon-mdx-gfm"],
+  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials")
+  , getAbsolutePath("@storybook/addon-mdx-gfm")],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: getAbsolutePath("@storybook/react-webpack5"),
     options: {}
   },
   webpackFinal: async (config, {
@@ -30,3 +30,7 @@ module.exports = {
     autodocs: true
   }
 };
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
