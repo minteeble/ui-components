40a7758905908f984bc84e965a4a95d4ce5c3213
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-01-04 09:07:41 +0100

    fix storybook

--- packages/ui-components-extra/.storybook/main.js
+++ packages/ui-components-extra/.storybook/main.js
@@ -1,34 +1,50 @@
 import { dirname, join } from "path";
 const path = require("path");
+
 module.exports = {
-  stories: ["../src/**/**/*.stories.mdx", "../src/**/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials")
-  , getAbsolutePath("@storybook/addon-mdx-gfm")],
+  stories: [
+    "../src/**/**/*.stories.mdx",
+    "../src/**/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+
+  addons: [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-mdx-gfm"),
+  ],
+
   framework: {
-    name: getAbsolutePath("@storybook/react-webpack5"),
-    options: {}
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {},
   },
-  webpackFinal: async (config, {
-    configType
-  }) => {
+
+  webpackFinal: async (config, { configType }) => {
     // get index of css rule
-    const ruleCssIndex = config.module.rules.findIndex(rule => rule.test.toString() === "/\\.css$/");
+    const ruleCssIndex = config.module.rules.findIndex(
+      (rule) => rule.test.toString() === "/\\.css$/"
+    );
 
     // map over the 'use' array of the css rule and set the 'module' option to true
-    config.module.rules[ruleCssIndex].use.map(item => {
+    config.module.rules[ruleCssIndex].use.map((item) => {
       if (item.loader && item.loader.includes("/css-loader/")) {
         item.options.modules = {
           mode: "local",
-          localIdentName: configType === "PRODUCTION" ? "[local]__[hash:base64:5]" : "[name]__[local]__[hash:base64:5]"
+          localIdentName:
+            configType === "PRODUCTION"
+              ? "[local]__[hash:base64:5]"
+              : "[name]__[local]__[hash:base64:5]",
         };
       }
+
       return item;
     });
+
     return config;
   },
+
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
 
 function getAbsolutePath(value) {
--- packages/ui-components-extra/.storybook/preview.js
+++ packages/ui-components-extra/.storybook/preview.js
@@ -1,5 +1,5 @@
 import "!style-loader!css-loader!sass-loader!../src/style/main.scss";
-// import "./custom.scss";
+import "./custom.scss";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
